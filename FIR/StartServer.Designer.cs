2a450a93ca7f6eb4d3a0d1be634082b55e3fdfe1
PotatoChipsNinja <shiyuhui2000@126.com>
2017-01-22 11:53:39 +0800

    将Connect和StartServer窗体的StartPosition改为CenterParent。

--- FIR/StartServer.Designer.cs
+++ FIR/StartServer.Designer.cs
@@ -85,6 +85,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "StartServer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Load += new System.EventHandler(this.StartServer_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
